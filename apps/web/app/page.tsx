5f3231f39fb4cbba4a4028085e37a816f1a45d8c
Utsav joshi <utsav304joshi@gmail.com>
2025-10-07 22:22:01 +0530

    setuped and made landing page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,18 @@
-export default function Home() {
+import { Nav } from "@/components/Navbar"
+import { Hero } from "@/components/HeroSection"
+
+export default function Page() {
   return (
-   <div>Hello World</div>
-  );
+    <main className="min-h-svh bg-background">
+      {/* Frame like the screenshot */}
+      <div className="mx-auto max-w-[1280px] p-3">
+        <div className="rounded-lg border-2 border-foreground/20">
+          <div className="relative">
+            <Nav />
+            <Hero />
+          </div>
+        </div>
+      </div>
+    </main>
+  )
 }
